34f96c94dad035c719db57aaa990da98b053f5d9
Matheus Moizeis <47633328+matheus-moizeis@users.noreply.github.com>
2021-06-09 16:15:19 -0300

    Configuração inicial, configuração do bootswatch

---
+++ src/views/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-3">Crud Lista de Produtos</h1>
+      <p className="lead">Trabalho 6º semestre Fatec-Franca</p>
+      <hr className="my-4" />
+      <p className="lead">
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Cadastrar
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/views/produtos/cadastro.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class CadastroProduto extends React.Component {
+    render(){
+        return(
+        <div className='card'>
+            <div className='card-header'>
+                Cadastro de Produto
+            </div>
+            <div className='card-body'>
+
+            </div>
+        </div>
+        )
+    }
+}
