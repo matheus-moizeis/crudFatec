f717a3ad43e0a20a9866bea87efc370f32a620fc
Matheus Moizeis <47633328+matheus-moizeis@users.noreply.github.com>
2021-06-09 16:25:13 -0300

    Configuração rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import Navbar from "./components/navbar";
-import Home from "./views/home";
+import Rotas from './rotas';
 
 function App() {
   return (
     <>
       <div className='container'>
         <Navbar />
-        <Home />
+        <Rotas />
       </div>
     </>
   );
